354e79d72115716f18aaa48b75200db70253c0c5
Isaque Gomes <isaquegeovani29@gmail.com>
2026-05-12 19:30:31 -0300

    chore: initial commit with spring boot configuration

---
+++ src/main/java/br/com/gomes/fit_tracker_app/FitTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomes.fit_tracker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gomes/fit_tracker_app/FitTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gomes.fit_tracker_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

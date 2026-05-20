3a418c2e2b1c0f5bd2a1824cd84ffc8f4a882169
Isaque Gomes <isaquegeovani29@gmail.com>
2026-05-20 13:13:25 -0300

    chore: removing unused import and adding Repository annotation

--- src/main/java/br/com/gomes/fit_tracker_app/repositories/UserRepository.java
+++ src/main/java/br/com/gomes/fit_tracker_app/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package br.com.gomes.fit_tracker_app.repositories;
 
 import br.com.gomes.fit_tracker_app.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById (User user);
 }

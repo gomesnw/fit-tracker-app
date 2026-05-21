3fce37776562839cd7e46acd3287e5c91b6e9093
Isaque Gomes <isaquegeovani29@gmail.com>
2026-05-21 17:45:48 -0300

    fix: adding profile configuration for test and production

--- src/main/java/br/com/gomes/fit_tracker_app/config/TestSecurityConfig.java
+++ src/main/java/br/com/gomes/fit_tracker_app/config/TestSecurityConfig.java
@@ -5,24 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile("prod")
-public class SecurityConfig {
+
+@Profile("test")
+public class TestSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChainTest(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers ->
-                        headers.frameOptions(frame -> frame.disable())
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();

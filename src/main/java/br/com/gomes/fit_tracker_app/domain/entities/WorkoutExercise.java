467c13020aee0cb3ae7929ae6ebb83c631e196ea
Isaque Gomes <isaquegeovani29@gmail.com>
2026-08-01 12:34:27 -0300

    refactor: implement WorkoutSet composition

--- src/main/java/br/com/gomes/fit_tracker_app/domain/entities/WorkoutExercise.java
+++ src/main/java/br/com/gomes/fit_tracker_app/domain/entities/WorkoutExercise.java
@@ -1,6 +1,7 @@
 package br.com.gomes.fit_tracker_app.domain.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -8,6 +9,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -40,11 +43,28 @@ public class WorkoutExercise implements Serializable {
     @Column(name="updated_at")
     private Instant updatedAt;
 
+    @NotNull
     @ManyToOne
-    @JoinColumn(name="exercise_id")
+    @JoinColumn(name="exercise_id", nullable = false)
     private Exercise exercise;
 
+    @NotNull
     @ManyToOne
-    @JoinColumn(name="workout_id")
+    @JoinColumn(name="workout_id", nullable = false)
     private Workout workout;
+
+    @OneToMany(mappedBy = "workoutExercise", cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<WorkoutSet> sets = new ArrayList<>();
+
+    public void addSet(WorkoutSet set) {
+        sets.add(set);
+        set.setWorkoutExercise(this);
+    }
+
+    public void removeSet(WorkoutSet set) {
+        sets.remove(set);
+        set.setWorkoutExercise(null);
+    }
+
 }

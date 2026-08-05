17df01e6c4d53f4b77d811a36142c92ddb1abd8b
Isaque Gomes <isaquegeovani29@gmail.com>
2026-08-05 16:35:02 -0300

    refactor: remove toEntity method and add exercises field

--- src/main/java/br/com/gomes/fit_tracker_app/dtos/WorkoutInsertDTO.java
+++ src/main/java/br/com/gomes/fit_tracker_app/dtos/WorkoutInsertDTO.java
@@ -1,13 +1,13 @@
 package br.com.gomes.fit_tracker_app.dtos;
 
-import br.com.gomes.fit_tracker_app.domain.entities.Workout;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
 
 public record WorkoutInsertDTO
         (@NotBlank String name,
+         @Size(min = 2) List <WorkoutExerciseInsertDTO> exercises,
          String notes)
-{
-    public Workout toEntity(){
-        return new Workout(null, null, null, null, name, null, notes, null, null, null);
-    }
-}
+{}

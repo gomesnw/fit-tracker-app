b2a83ee246afe7f2e6e302ad42826eec807c1f96
Isaque Gomes <isaquegeovani29@gmail.com>
2026-08-16 16:26:31 -0300

    fix: remove username field

--- src/main/java/br/com/gomes/fit_tracker_app/dtos/UserResponseDTO.java
+++ src/main/java/br/com/gomes/fit_tracker_app/dtos/UserResponseDTO.java
@@ -7,13 +7,12 @@ import java.time.Instant;
 
 public record UserResponseDTO(
         Long id,
-        String username,
         String name,
         String email,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Sao_Paulo")
         Instant createdAt
 ){
     public UserResponseDTO(User entity) {
-        this(entity.getId(), entity.getUsername(), entity.getName(), entity.getEmail(), entity.getCreatedAt());
+        this(entity.getId(), entity.getName(), entity.getEmail(), entity.getCreatedAt());
     }
 }
--- src/main/java/br/com/gomes/fit_tracker_app/dtos/UserSummaryDTO.java
+++ src/main/java/br/com/gomes/fit_tracker_app/dtos/UserSummaryDTO.java
@@ -2,10 +2,10 @@ package br.com.gomes.fit_tracker_app.dtos;
 
 import br.com.gomes.fit_tracker_app.domain.entities.User;
 
-public record UserSummaryDTO(Long id, String name, String username) {
+public record UserSummaryDTO(Long id, String name) {
 
     public UserSummaryDTO (User entity){
-        this(entity.getId(), entity.getName(), entity.getUsername());
+        this(entity.getId(), entity.getName());
     }
 }
 

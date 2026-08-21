862194ab06528b937329af33264551f7552d9d81
Isaque Gomes <isaquegeovani29@gmail.com>
2026-08-20 21:11:00 -0300

    refactor: simplify authenticated user retrieval

--- src/main/java/br/com/gomes/fit_tracker_app/controllers/UserController.java
+++ src/main/java/br/com/gomes/fit_tracker_app/controllers/UserController.java
@@ -1,51 +1,29 @@
 package br.com.gomes.fit_tracker_app.controllers;
 
-import br.com.gomes.fit_tracker_app.dtos.UserInsertDTO;
 import br.com.gomes.fit_tracker_app.dtos.UserResponseDTO;
 import br.com.gomes.fit_tracker_app.dtos.UserUpdateDTO;
 import br.com.gomes.fit_tracker_app.dtos.UserUpdateResponseDTO;
 import br.com.gomes.fit_tracker_app.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping(value="/v1/users")
 public class UserController {
     private final UserService userService;
 
+    @PutMapping
+    public ResponseEntity<UserUpdateResponseDTO> updateMe(@RequestBody @Valid UserUpdateDTO userUpdate) {
+        return ResponseEntity.ok()
+                .body(userService.updateMe(userUpdate));
+    }
+
     @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> findAll(){
-       List<UserResponseDTO> usersList = userService.findALl();
-       return ResponseEntity.ok().body(usersList);
-    }
-
-    @GetMapping(value= "/{id}")
-    public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id){
-        UserResponseDTO user = userService.findById(id);
-        return ResponseEntity.ok(user);
-    }
-
-    @PostMapping
-    public ResponseEntity<UserResponseDTO> insertUser (@RequestBody @Valid UserInsertDTO userInsert){
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(userService.insertUser(userInsert));
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<UserUpdateResponseDTO> updateUser (@PathVariable Long id, @RequestBody @Valid UserUpdateDTO userUpdate){
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(userService.updateUser(id, userUpdate));
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> deleteUser (@PathVariable Long id){
-        userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<UserResponseDTO> findMe(){
+        return ResponseEntity.ok().
+                body(userService.findMe());
     }
 }
--- src/main/java/br/com/gomes/fit_tracker_app/services/UserService.java
+++ src/main/java/br/com/gomes/fit_tracker_app/services/UserService.java
@@ -1,61 +1,36 @@
 package br.com.gomes.fit_tracker_app.services;
 
 import br.com.gomes.fit_tracker_app.domain.entities.User;
-import br.com.gomes.fit_tracker_app.dtos.UserInsertDTO;
 import br.com.gomes.fit_tracker_app.dtos.UserResponseDTO;
 import br.com.gomes.fit_tracker_app.dtos.UserUpdateDTO;
 import br.com.gomes.fit_tracker_app.dtos.UserUpdateResponseDTO;
-import br.com.gomes.fit_tracker_app.exceptions.ResourceNotFoundException;
 import br.com.gomes.fit_tracker_app.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private static final String MSG_USER_NOT_FOUND = "Usuário não encontrado com o id fornecido: %d";
+    private final AuthenticationService authenticationService;
 
-    public List <UserResponseDTO> findALl(){
-        List <User> usersList = userRepository.findAll();
-        return usersList.stream().map(UserResponseDTO::new).toList();
-    }
-
-    public UserResponseDTO findById (Long id){
-        User entity = userRepository.findById(id)
-                .orElseThrow(() ->new ResourceNotFoundException(String.format(MSG_USER_NOT_FOUND, id)));
+    public UserResponseDTO findMe(){
+        User entity = authenticationService.getAuthenticatedUser();
         return new UserResponseDTO(entity);
     }
 
-    public UserResponseDTO insertUser (UserInsertDTO userInsert){
-        User entity = userInsert.toEntity();
-        entity = userRepository.save(entity);
-        return new UserResponseDTO(entity);
-    }
+    public UserUpdateResponseDTO updateMe (UserUpdateDTO userUpdate){
+        User existentUser = authenticationService.getAuthenticatedUser();
 
-    public UserUpdateResponseDTO updateUser (Long id, UserUpdateDTO userUpdate){
-        User existentUser = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format(MSG_USER_NOT_FOUND, id)));
         updateUserData(existentUser, userUpdate);
         existentUser = userRepository.save(existentUser);
 
         return new UserUpdateResponseDTO(existentUser);
     }
 
-    public void deleteUser (Long id){
-        User entity = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format(MSG_USER_NOT_FOUND, id)));
-
-        userRepository.delete(entity);
-    }
-
     private void updateUserData(User entity, UserUpdateDTO updateInsertDTO) {
         entity.setName(updateInsertDTO.name());
-        entity.setEmail(updateInsertDTO.email());
-        entity.setPhone(updateInsertDTO.phone());
     }
 }
 
